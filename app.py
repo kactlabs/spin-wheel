b8e054c8d902de7d10f38499b9197d6d2a23f49b
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-25 05:21:20 +0530

    Jsonified the Input Data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import json
 
 
 app = Flask(__name__)
@@ -9,10 +10,19 @@ def home():
     return render_template('front.html')
 
 
-@app.route("/wheel", methods = ['GET', 'POST'])
-def wheel():
-    
-    return render_template("index.html")
+@app.route('/wheel', methods = ['GET', 'POST'])
+def hello():
+    json = get_json('data.json')
+    data = json["user_data"]
+    return render_template('index.html', values=data)
+
+def get_json(USERS_JSONPATH):
+
+    json_file = open(USERS_JSONPATH)
+    json_data = json.load(json_file)
+    #print(json_data)
+
+    return json_data
 
 if __name__== "__main__":
     app.run(debug=True)
