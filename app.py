89724c559209d12053fe6259ed2e3c90edf047d3
Raja CSP Raman <raja.csp@gmail.com>
2021-05-25 09:17:26 -0400

    Dynamic users provided from Python

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
+
+'''
+Created on 
+    
+
+Course work: 
+    
+
+@author: 
+    Ana Jessica
+    Ishita
+
+Source:
+    
+'''
+
+# Import necessary modules
 from flask import Flask, request, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('front.html')
 
-
 @app.route("/wheel", methods = ['GET', 'POST'])
 def wheel():
+
+    user_list = [
+        "Ana", 
+        "Vedha",
+        "Ishita",
+        "Gokul",
+        "Prakash",
+        "Elakia"
+    ]
+
+    user_string = ','.join(user_list)
     
-    return render_template("index.html")
+    return render_template("index.html", user_str = user_string)
 
 if __name__== "__main__":
     app.run(debug=True)
