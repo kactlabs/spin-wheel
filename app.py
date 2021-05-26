758219d37dfc254ae656a630df3b390ea2812f3e
K. Ana Jessica <anajessica15022002@gmail.com>
2021-05-26 18:03:49 +0530

    updated

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ Source:
 
 # Import necessary modules
 from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
@@ -42,6 +43,35 @@ def wheel():
     user_string = ','.join(user_list)
     
     return render_template("index.html", user_str = user_string)
+
+
+@app.route("/admin", methods = ['GET'])
+def admin():
+    
+    return render_template("admin.html")
+
+@app.route('/admin/post',methods=['POST'])
+def wheelnames():
+    
+    profile_name=request.form.get("wheel-names")
+    print (profile_name)
+
+    arr = []
+    arr = profile_name.splitlines()
+
+    my_dict = dict() 
+    for index,value in enumerate(arr):
+        my_dict[index] = value
+
+    print(json.dumps(my_dict))
+
+    with open("users.json", "w") as outfile:
+        outfile.write(json.dumps(my_dict))
+
+    return render_template('admin.html')
+    
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
