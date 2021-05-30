9912a1ffc810e2e54980786f1e07bdf48caf374f
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-30 13:07:22 +0530

    DID STUFF

--- app.py
+++ app.py
@@ -25,34 +25,9 @@ USERS_JSONPATH = "data.json"
 def home():
     return render_template('front.html')
 
-
-# @app.route("/wheel", methods = ['GET', 'POST'])
-# def wheel():
-
-#     user_list = [
-#         "Ana", 
-#         "Vedha",
-#         "Gokul",
-#         "Prakash",
-#         "Elakia",
-#         "Divya",
-#         "Aswin",
-#         "Praabindh",
-#         "Ishita",
-#         "Mohit",
-#         "Sanjana",
-#         "praveena"
-
-#     ]
-
-#     user_string = ','.join(user_list)
-    
-#     return render_template("index.html", user_str = user_string)
-
-
 @app.route('/wheel', methods = ['GET', 'POST'])
 def hello():
-    json = get_json('data.json')
+    json = get_json()
     data = json["user_data"]
     return render_template('index.html', values=data)
 
@@ -71,8 +46,60 @@ def get_json():
 
     return json_data
 
-@app.route('/admin/view',methods=['GET','POST'])
-def get_json():
+@app.route('/admin/post',methods=['GET','POST'])
+def add_json():
+
+    new_value = request.values.get("wheel-names")
+
+    data_dict = get_json()
+
+    data_list = data_dict["user_data"]
+
+    data_list.reverse()
+
+    if len(data_list) == 0:
+        num = 0
+    else:
+        num =  data_list[0]["value"]
+
+    new_value_dict = {
+        
+            "label": new_value,
+            "value": num + 1 , 
+            "question": new_value
+        
+    }
+
+    data_list.reverse()
+
+    data_list.append(new_value_dict)
+
+    updated_data_dict = {
+        "user_data": data_list
+    }
+
+    with open(USERS_JSONPATH, 'w') as outfile:
+        json.dump(updated_data_dict, outfile)
+
+    return render_template("admin.html")
+
+@app.route('/admin/flush',methods=['GET','POST'])
+def flush_json():
+
+    data_dict = get_json()
+
+    data_list = data_dict["user_data"]
+
+    data_list = []
+
+    empty_data_dict = {
+        "user_data": data_list
+    }
+
+    with open(USERS_JSONPATH, 'w') as outfile:
+        json.dump(empty_data_dict, outfile)
+
+    return render_template("admin.html")
 
 if __name__== "__main__":
     app.run(debug=True)
