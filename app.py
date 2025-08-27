dd8576949bf6313f61f5938a42841c80af85ea8a
Raja CSP Raman <raja.csp@gmail.com>
2025-08-27 19:08:29 +0530

    Converted to FastAPI

--- app.py
+++ app.py
@@ -15,15 +15,18 @@ Source:
 '''
 
 # Import necessary modules
-from flask import Flask, request, render_template
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import json
 
-app = Flask(__name__)
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 USERS_JSONPATH = "data.json"
 
-@app.route('/')
-def home():
-    return render_template('front.html')
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse('front.html', {"request": request})
 
 
 # @app.route("/wheel", methods = ['GET', 'POST'])
@@ -50,31 +53,27 @@ def home():
 #     return render_template("index.html", user_str = user_string)
 
 
-@app.route('/wheel', methods = ['GET', 'POST'])
-def hello():
+@app.get('/wheel', response_class=HTMLResponse)
+@app.post('/wheel', response_class=HTMLResponse)
+def hello(request: Request):
     json_data = get_json()
     data = json_data["user_data"]
-    return render_template('index.html', values=data)
+    return templates.TemplateResponse('index.html', {"request": request, "values": data})
 
 
-@app.route("/admin", methods = ['GET'])
-def admin():
-    
-    return render_template("admin.html")
+@app.get("/admin", response_class=HTMLResponse)
+def admin(request: Request):
+    return templates.TemplateResponse("admin.html", {"request": request})
 
-@app.route('/admin/post',methods=['GET','POST'])
+@app.get('/admin/post')
+@app.post('/admin/post')
 def get_json():
-
-    json_file = open(USERS_JSONPATH)
-    json_data = json.load(json_file)
-    #print(json_data)
-
+    with open(USERS_JSONPATH) as json_file:
+        json_data = json.load(json_file)
     return json_data
 
 # return render_template('admin.html')
 
-if __name__== "__main__":
-    app.run(
-        port = 5006,
-        debug = True
-    )
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5006)
